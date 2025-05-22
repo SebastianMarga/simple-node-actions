70576d70fa75134fe008b90ac7699af9ad7dda92
SebastianMarga <vsmv0205gmail.com>
2025-05-22 11:11:23 -0500

    Primer cambio

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function add(a,b){
-    return a+b;
+function mul(a,b){
+    return a*b;
 }
-module.exports={add}; 
+module.exports={mul}; 
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,5 @@
-const {add} = require('./index');
+const {mul} = require('./index');
 
-test('adds 1+2 to equal 3', ()=> {
-    expect(add(1,2)).toBe(3);
+test('adds 1*2 to equal 2', ()=> {
+    expect(add(1,2)).toBe(2);
 })
